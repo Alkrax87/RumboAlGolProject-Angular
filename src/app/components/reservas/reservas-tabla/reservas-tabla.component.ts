73d43637b3f72a5c5506a2494667975f21ff2b64
Alkrax87 <thedarkdawn87gtamc@gmail.com>
2024-08-01 17:49:15 -0500

    Implementacion de play-off en torneo de reserva

--- src/app/components/reservas/reservas-tabla/reservas-tabla.component.ts
+++ src/app/components/reservas/reservas-tabla/reservas-tabla.component.ts
@@ -20,13 +20,47 @@ export class ReservasTablaComponent {
   sur:any;  // Data grupo sur
 
   // Objetos clasificados
-  clasifiers:any;
+  octavos:any;
+  cuartos:any;
+  semifinalist:any;
+  finalist:any;
+  champion:any;
 
   loadData() {
     this.dataLoadService.loadData(6).then((data: any) => {
       this.data = data;
       this.norte = this.sortDataTeamsNorte([...data.Teams]);
       this.sur = this.sortDataTeamsSur([...data.Teams]);
+
+      // Playoff
+      if (data.Info.playoff){
+        this.selectClasifiers([...this.norte],[...this.sur]);
+      }
+
+      // Cuartos
+      if (data.Info.cuartos){
+        this.octavosSelector();
+      }
+
+      // Semifinal
+      if (data.Info.semifinal) {
+        this.semifinalSelector();
+      }
+
+      // Final
+      if (data.Info.final) {
+        this.finalSelector();
+        if (data.Final.teamA != null && data.Final.teamB != null) {
+          let winner = this.bracketWinner(0,data.Final.teamA,data.Final.penal.teamA,1,data.Final.teamB,data.Final.penal.teamB)
+
+          if (winner == 0) {
+            this.champion = this.finalist[0];
+          } else {
+            this.champion = this.finalist[1];
+          }
+        }
+      }
+
     }).catch(error => {
       console.error('Error al cargar datos en el componente:', error);
     });
@@ -144,7 +178,63 @@ export class ReservasTablaComponent {
     return teamsArray;
   }
 
+  // Seleccionar Clasificados
+  selectClasifiers(dataN:any, dataS:any){
+    let octavos = [];
+    let cuartos = [];
+    for (let i = 0; i < 6; i++) {
+      if (i < 2) {
+        cuartos.push(dataN[i])
+        cuartos.push(dataS[i])
+      } else {
+        octavos.push(dataN[i])
+        octavos.push(dataS[i])
+      }
+    }
+    this.octavos = octavos;
+    this.cuartos = cuartos;
+  }
 
+  // Funcion que calcula que equipo clasifica en cada bracket
+  bracketWinner(teamAIndex:number, goalsA:number, penaltyA:number, teamBIndex:number, goalsB:number, penaltyB:number){
+    if (penaltyA && penaltyB) {
+      if (penaltyA > penaltyB) {
+        return teamAIndex;
+      } else {
+        return teamBIndex;
+      }
+    } else {
+      if (goalsA > goalsB) {
+        return teamAIndex;
+      } else {
+        return teamBIndex;
+      }
+    }
+  }
 
+  // Calcular resultados Octavos y seleccionar clasificados
+  octavosSelector(){
+    this.cuartos.push(this.octavos[this.bracketWinner(3,(this.data.Octavos[0].teamA + 0),this.data.Octavos[0].penal.TeamA,4,(this.data.Octavos[0].teamB + 0),this.data.Octavos[0].penal.TeamB)])
+    this.cuartos.push(this.octavos[this.bracketWinner(0,(this.data.Octavos[1].teamA + 0),this.data.Octavos[1].penal.TeamA,7,(this.data.Octavos[1].teamB + 0),this.data.Octavos[1].penal.TeamB)])
+    this.cuartos.push(this.octavos[this.bracketWinner(2,(this.data.Octavos[2].teamA + 0),this.data.Octavos[2].penal.TeamA,5,(this.data.Octavos[2].teamB + 0),this.data.Octavos[2].penal.TeamB)])
+    this.cuartos.push(this.octavos[this.bracketWinner(1,(this.data.Octavos[3].teamA + 0),this.data.Octavos[3].penal.TeamA,6,(this.data.Octavos[3].teamB + 0),this.data.Octavos[3].penal.TeamB)])
+  }
 
+  // Calcular resultados Cuartos y seleccionar clasificados
+  semifinalSelector(){
+    let clasifiers = [];
+    clasifiers.push(this.cuartos[this.bracketWinner(0,(this.data.Cuartos[0].idaA + this.data.Cuartos[0].vueltaA),this.data.Cuartos[0].penal.teamA,4,(this.data.Cuartos[0].idaB + this.data.Cuartos[0].vueltaB),this.data.Cuartos[0].penal.teamB)]);
+    clasifiers.push(this.cuartos[this.bracketWinner(3,(this.data.Cuartos[1].idaA + this.data.Cuartos[1].vueltaA),this.data.Cuartos[1].penal.teamA,5,(this.data.Cuartos[1].idaB + this.data.Cuartos[1].vueltaB),this.data.Cuartos[1].penal.teamB)]);
+    clasifiers.push(this.cuartos[this.bracketWinner(1,(this.data.Cuartos[2].idaA + this.data.Cuartos[2].vueltaA),this.data.Cuartos[2].penal.teamA,6,(this.data.Cuartos[2].idaB + this.data.Cuartos[2].vueltaB),this.data.Cuartos[2].penal.teamB)]);
+    clasifiers.push(this.cuartos[this.bracketWinner(2,(this.data.Cuartos[3].idaA + this.data.Cuartos[3].vueltaA),this.data.Cuartos[3].penal.teamA,7,(this.data.Cuartos[3].idaB + this.data.Cuartos[3].vueltaB),this.data.Cuartos[3].penal.teamB)]);
+    this.semifinalist = clasifiers;
+  }
+
+  // Calcular resultados Cuartos y seleccionar clasificados
+  finalSelector(){
+    let clasifiers = [];
+    clasifiers.push(this.semifinalist[this.bracketWinner(0,(this.data.Semifinal[0].idaA + this.data.Semifinal[0].vueltaA),this.data.Semifinal[0].penal.teamA,1,(this.data.Semifinal[0].idaB + this.data.Semifinal[0].vueltaB),this.data.Semifinal[0].penal.teamB)]);
+    clasifiers.push(this.semifinalist[this.bracketWinner(2,(this.data.Semifinal[1].idaA + this.data.Semifinal[1].vueltaA),this.data.Semifinal[1].penal.teamA,3,(this.data.Semifinal[1].idaB + this.data.Semifinal[1].vueltaB),this.data.Semifinal[1].penal.teamB)]);
+    this.finalist = clasifiers;
+  }
 }
